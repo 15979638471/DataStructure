f9a53241f9607d8af3c6856bb653aa020166aaad
Mr.Liao <zxc1106190679@qq.com>
2020-10-26 10:50:15 +0800

    完成二分查找算法禾插值查找算法

--- src/com/ryou/search/InterpolationSearch.java
+++ src/com/ryou/search/InterpolationSearch.java
@@ -2,12 +2,15 @@ package com.ryou.search;
 
 import java.util.Arrays;
 
-public class BinarySearch {
+public class InterpolationSearch {
 	
 	public static int search(int[] arr, int val) {
+		if(val < arr[0] || val > arr[arr.length - 1]) {
+			return -1;
+		}
 		int front = 0;
 		int end = arr.length - 1;
-		int mid = (front + end) / 2;
+		int mid = front + (end - front) * (val - arr[front]) / (arr[end] - arr[front]);
 		while(front <= end) {
 			if(val == arr[mid]) {
 				return mid;
@@ -16,15 +19,15 @@ public class BinarySearch {
 			}else if(val > arr[mid]) {
 				front = mid + 1;
 			}
-			mid = (front + end) / 2;
+			mid = front + (end - front) * (val - arr[front]) / (arr[end] - arr[front]);
 		}
 		return -1;
 	}
 	
 	public static void main(String[] args) {
-		int[] arr = {43,22,15,35,33,24,16,77,86,94};
+		int[] arr = {43,22,15,35,33,24,16,77,86,94,-1,-2};
 		Arrays.sort(arr);
 		System.out.println(Arrays.toString(arr));
-		System.out.println(search(arr, 1000));
+		System.out.println(search(arr, -1));
 	}
 }
